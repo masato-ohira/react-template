56f140de4dced222ddaa07987a06b5cf2e8ca562
Masato Ohira <ohira.ekzm@gmail.com>
2022-09-29 08:39:09 +0900

    init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('app-name') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
