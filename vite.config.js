21ebcec249888fadeed8be6c4ebd6151591e061f
Masato Ohira <ohira.ekzm@gmail.com>
2022-12-23 09:29:52 +0900

    fix:vite-config

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,17 @@ export default defineConfig({
     react({
       jsxImportSource: '@emotion/react',
       babel: {
-        plugins: ['@emotion/babel-plugin'],
+        plugins: [
+          [
+            '@emotion',
+            {
+              // sourceMap: true,
+              autoLabel: 'always',
+              labelFormat: '[filename]-[local]',
+              cssPropOptimization: true,
+            },
+          ],
+        ],
       },
     }),
   ],
